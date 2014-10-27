cdd4cd23670f41f05a0cd6b8017e129a4ea0c46b
felixaa <chris.felixaa@gmail.com>
2014-10-27 14:38:36 +0100

    Might be an issue with gitignore files

--- app/src/main/java/com/felixaa/fest_bong/LoginActivity.java
+++ app/src/main/java/com/felixaa/fest_bong/LoginActivity.java
@@ -29,6 +29,7 @@ import java.util.List;
 
 /**
  * Created by Chris on 10/2/2014.
+ * Vil ikke fungere når man trykker LOG in, fortsatt manglende funksjon
  */
 public class LoginActivity extends Activity implements OnClickListener{
 
